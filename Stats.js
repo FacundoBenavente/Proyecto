56e9fe4a5acc3064dd1a5e4f0dc713dd153629c0
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2024-08-19 10:45:39 -0300

    Futbots 1.21
    Avancé con la pantalla  de organizar torneo y de cantidad de jugadores e intenté programar el AND de la pantalla de stats (no salió)

---
+++ Stats.js
@@ -0,0 +1,14 @@
+var boton1 = "";
+var boton2 = "";
+
+function Button1(){
+        boton1 = "SI";
+}
+function Button2(){
+        boton2 = "SI";
+}
+
+if(boton1 == "SI" && boton2 == "SI"){
+    window.location = "Partido.html";
+
+}
