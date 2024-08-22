282287590b23c58dd2051373f1e7e145f76ca928
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2024-08-22 20:03:12 -0300

    Futbots 1.43

--- Login.js
+++ Login.js
@@ -1,3 +1,21 @@
 function Register1(){
+    if(title1.innerHTML == "Login"){
+    submitBtn1.value = "Registrarse";
     title1.innerHTML = "Registrarse";
+    }
+}
+
+function crearCuenta(){
+var usuario = {
+    "email": txtEmail.value,
+    "password": txtPass.value
+}
+var jsonUser = JSON.stringify(usuario);
+localStorage.setItem(txtEmail.value, jsonUser);
+}
+function clickLogin(){
+    if(submitBtn1.value == "Registrarse"){
+        crearCuenta();
+    }
+
 }
