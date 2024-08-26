24ae52ad82ee600942ff54024e184ab9d98c3e36
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2024-08-26 10:39:07 -0300

    Futbots 1.44
    Termine lo de registrarse y me falta hacer funcionar el login, o almenos poner que me lleve a la siguiente página y después seguir con eso.

--- Login.js
+++ Login.js
@@ -2,20 +2,71 @@ function Register1(){
     if(title1.innerHTML == "Login"){
     submitBtn1.value = "Registrarse";
     title1.innerHTML = "Registrarse";
+    registrarse1.innerHTML = "¿Ya tienes cuenta? Inicia sesion"
+    userName1.hidden = false;
+    txtName.type = "text";
+    } else{
+        submitBtn1.value = "Login";
+        title1.innerHTML = "Login";
+        registrarse1.innerHTML = "¿No tienes cuenta? Registrate"
+        userName1.hidden = true;
+        txtName.type = "hidden";
+    }
+}
+function Register2(){
+    if(title2.innerHTML == "Login"){
+    submitBtn2.value = "Registrarse";
+    title2.innerHTML = "Registrarse";
+    registrarse2.innerHTML = "¿Ya tienes cuenta? Inicia sesion"
+    userName2.hidden = false;
+    txtName2.type = "text";
+    } else{
+        submitBtn2.value = "Login";
+        title2.innerHTML = "Login";
+        registrarse2.innerHTML = "¿No tienes cuenta? Registrate"
+        userName2.hidden = true;
+        txtName2.type = "hidden";
     }
 }
 
 function crearCuenta(){
 var usuario = {
     "email": txtEmail.value,
-    "password": txtPass.value
+    "password": txtPass.value,
+    "username": txtName.value
 }
 var jsonUser = JSON.stringify(usuario);
 localStorage.setItem(txtEmail.value, jsonUser);
 }
+
+/*function loginCuenta(){
+ let userLog= localStorage.getItem(txtEmail.value);
+ if(txtPass == ){
+     listo.hidden = false;
+     listo.innerHTML = "¡Listo!";
+ } else{
+     listo.hidden = false;
+     listo.innerHTML = "contraseña incorrecta";
+ }
+}*/
+function crearCuenta2(){
+    var usuario = {
+        "email": txtEmail2.value,
+        "password": txtPass2.value,
+        "username": txtName2.value
+        }
+
+var jsonUser = JSON.stringify(usuario);
+localStorage.setItem(txtEmail2.value, jsonUser);
+    }
 function clickLogin(){
     if(submitBtn1.value == "Registrarse"){
         crearCuenta();
+    } else{
+        loginCuenta();
     }
+    if(submitBtn2.value == "Registrarse"){
+        crearCuenta2();
+    } 
 
 }
